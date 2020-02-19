f96e5a48e7cdaff3457916e10eac24b51fa9cd9b
Christian Gulden <christian.gulden@fau.de>
2020-02-19 21:24:14 +0000

    fix: revert explicit specification of response content type

--- src/main/java/org/miracum/etl/fhirgateway/controllers/FhirController.java
+++ src/main/java/org/miracum/etl/fhirgateway/controllers/FhirController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/fhir", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/fhir")
 @ResponseBody
 public class FhirController {
 
