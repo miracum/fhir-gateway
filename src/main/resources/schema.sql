2678b7db572d9788fe8d31836af13b8a12795136
Christian Gulden <christian.gulden@fau.de>
2020-03-09 15:46:43 +0100

    feat: create indices to speed up query performance

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS resources
     type       varchar(64) NOT NULL,
     data       jsonb       NOT NULL,
     created_at timestamp   NOT NULL DEFAULT NOW(),
-    CONSTRAINT fhir_id_unique
-        UNIQUE (fhir_id)
+    CONSTRAINT fhir_id_unique UNIQUE (fhir_id)
 );
+
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS resource_id_idx ON resources (id);
+CREATE INDEX IF NOT EXISTS resource_type_idx ON resources (type);
