104d856989e3ef72a426fad9b897536927b067d2
Christian Gulden <christian.gulden@fau.de>
2020-11-09 19:42:59 +0100

    feat: improved conversion from single resource to bundle

--- src/main/java/org/miracum/etl/fhirgateway/controllers/FhirController.java
+++ src/main/java/org/miracum/etl/fhirgateway/controllers/FhirController.java
@@ -4,9 +4,11 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Map;
 import java.util.UUID;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
 import org.hl7.fhir.r4.model.Resource;
 import org.miracum.etl.fhirgateway.processors.ResourcePipeline;
 import org.slf4j.Logger;
@@ -15,7 +17,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(
@@ -70,17 +76,27 @@ public class FhirController {
 
   @RequestMapping(
       value = {"/{resourceName}", "/{resourceName}/{id}"},
-      method = {RequestMethod.PUT, RequestMethod.POST})
-  public ResponseEntity<String> postPutFhirResource(
-      @PathVariable(value = "resourceName") String resourceName,
-      @PathVariable(value = "id", required = false) String id,
-      @RequestBody String body) {
+      method = RequestMethod.POST)
+  public ResponseEntity<String> postResource(@RequestBody String body) {
+    return handlePostPutResource(body, RequestMethod.POST);
+  }
+
+  @RequestMapping(
+      value = {"/{resourceName}", "/{resourceName}/{id}"},
+      method = RequestMethod.PUT)
+  public ResponseEntity<String> putResource(@RequestBody String body) {
+    return handlePostPutResource(body, RequestMethod.PUT);
+  }
+
+  private ResponseEntity<String> handlePostPutResource(String body, RequestMethod method) {
     if (body == null) {
       log.debug("Request body is empty");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    var resource = fhirParser.parseResource(body);
+    var httpMethodMap = Map.of(RequestMethod.POST, HTTPVerb.POST, RequestMethod.PUT, HTTPVerb.PUT);
+
+    var resource = (Resource) fhirParser.parseResource(body);
 
     var bundle = new Bundle();
     if (resource instanceof Bundle) {
@@ -88,7 +104,13 @@ public class FhirController {
     } else {
       bundle.setType(BundleType.TRANSACTION);
       bundle.setId(UUID.randomUUID().toString());
-      bundle.addEntry().setResource((Resource) resource);
+      bundle
+          .addEntry()
+          .setResource(resource)
+          .setFullUrl(resource.getId())
+          .getRequest()
+          .setMethod(httpMethodMap.getOrDefault(method, HTTPVerb.PUT))
+          .setUrl(resource.getId());
     }
 
     var processed = pipeline.process(bundle);
