873d3151ca6e2ffd5a0e9e1069b6f1934f750a2f
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-03-22 12:40:14 +0100

    feat: partitioning by default

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,7 +8,17 @@ CREATE TABLE IF NOT EXISTS resources
     last_updated_at timestamp   NOT NULL DEFAULT NOW(),
     is_deleted      boolean     NOT NULL DEFAULT FALSE,
     CONSTRAINT fhir_id_unique UNIQUE (fhir_id, type)
-);
+) PARTITION BY LIST (type);
+
+CREATE TABLE resources_patient PARTITION OF resources FOR VALUES IN ('Patient');
+CREATE TABLE resources_encounter PARTITION OF resources FOR VALUES IN ('Encounter');
+CREATE TABLE resources_condition PARTITION OF resources FOR VALUES IN ('Condition');
+CREATE TABLE resources_observation PARTITION OF resources FOR VALUES IN ('Observation');
+CREATE TABLE resources_medication PARTITION OF resources FOR VALUES IN ('Medication');
+CREATE TABLE resources_medication_statement PARTITION OF resources FOR VALUES IN ('MedicationStatement');
+CREATE TABLE resources_medication_administration PARTITION OF resources FOR VALUES IN ('MedicationAdministration');
+CREATE TABLE resources_procedure PARTITION OF resources FOR VALUES IN ('Procedure');
+CREATE TABLE resources_others PARTITION OF resources DEFAULT;
 
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS resource_id_idx ON resources (id);
 CREATE INDEX IF NOT EXISTS resource_type_idx ON resources (type);
