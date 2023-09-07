db98fcf8877f7d2a0688cbc618a14e4eac15af79
Marvin Kampf <marvin.kampf@uk-erlangen.de>
2023-09-07 13:22:53 +0200

    refactor: removes Jaeger Tracing and updates to Gradle 8.2

--- src/main/java/org/miracum/etl/fhirgateway/AppConfig.java
+++ src/main/java/org/miracum/etl/fhirgateway/AppConfig.java
@@ -8,23 +8,12 @@ import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import ca.uhn.fhir.rest.server.exceptions.ResourceVersionConflictException;
-import io.jaegertracing.internal.propagation.TraceContextCodec;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener;
-import io.opentracing.Span;
-import io.opentracing.contrib.java.spring.jaeger.starter.TracerBuilderCustomizer;
-import io.opentracing.contrib.okhttp3.OkHttpClientSpanDecorator;
-import io.opentracing.contrib.okhttp3.TracingInterceptor;
-import io.opentracing.propagation.Format;
-import io.opentracing.util.GlobalTracer;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-import okhttp3.Connection;
 import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.miracum.etl.fhirgateway.processors.FhirPseudonymizer;
 import org.miracum.etl.fhirgateway.processors.IPseudonymizer;
 import org.miracum.etl.fhirgateway.processors.NoopPseudonymizer;
@@ -32,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -60,21 +48,6 @@ public class AppConfig {
           boolean useLoadBalancerConnectionPool) {
     var fhirContext = FhirContext.forR4();
 
-    var opNameDecorator =
-        new OkHttpClientSpanDecorator() {
-          @Override
-          public void onRequest(Request request, Span span) {
-            // add the operation name to the span
-            span.setOperationName(request.url().encodedPath());
-          }
-
-          @Override
-          public void onError(Throwable throwable, Span span) {}
-
-          @Override
-          public void onResponse(Connection connection, Response response, Span span) {}
-        };
-
     var connectionPool = new ConnectionPool();
     if (useLoadBalancerConnectionPool) {
       connectionPool =
@@ -82,15 +55,8 @@ public class AppConfig {
               MAX_IDLE_CONNECTIONS, KEEP_ALIVE_DURATION_MILLISECONDS, TimeUnit.MILLISECONDS);
     }
 
-    var tracingInterceptor =
-        new TracingInterceptor(
-            GlobalTracer.get(),
-            Arrays.asList(OkHttpClientSpanDecorator.STANDARD_TAGS, opNameDecorator));
-
     var okclient =
         new OkHttpClient.Builder()
-            .addInterceptor(tracingInterceptor)
-            .addNetworkInterceptor(tracingInterceptor)
             .connectionPool(connectionPool)
             .eventListener(
                 OkHttpMetricsEventListener.builder(Metrics.globalRegistry, "fhir.client").build())
@@ -101,21 +67,6 @@ public class AppConfig {
 
     fhirContext.setRestfulClientFactory(okHttpFactory);
     return fhirContext;
-  }
-
-  @Bean
-  public TracerBuilderCustomizer traceContextJaegerTracerCustomizer() {
-    return builder -> {
-      var injector = new TraceContextCodec.Builder().build();
-
-      builder
-          .registerInjector(Format.Builtin.HTTP_HEADERS, injector)
-          .registerExtractor(Format.Builtin.HTTP_HEADERS, injector);
-
-      builder
-          .registerInjector(Format.Builtin.TEXT_MAP, injector)
-          .registerExtractor(Format.Builtin.TEXT_MAP, injector);
-    };
   }
 
   @Bean
@@ -204,14 +155,5 @@ public class AppConfig {
     } else {
       return new NoopPseudonymizer();
     }
-  }
-
-  @Bean
-  @ConditionalOnProperty(
-      value = "opentracing.jaeger.enabled",
-      havingValue = "false",
-      matchIfMissing = false)
-  public io.opentracing.Tracer jaegerTracer() {
-    return io.opentracing.noop.NoopTracerFactory.create();
   }
 }
