db9bb33a277dc1436b2dbf684626cf7d1faaa8fc
Marvin Kampf <marvin.kampf@uk-erlangen.de>
2025-05-16 22:28:02 +0200

    test: adds embedded kafka to test context load sucessfully

--- src/test/java/org/miracum/etl/fhirgateway/FhirGatewayApplicationTests.java
+++ src/test/java/org/miracum/etl/fhirgateway/FhirGatewayApplicationTests.java
@@ -2,10 +2,12 @@ package org.miracum.etl.fhirgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EmbeddedKafka(brokerProperties = {"listeners=PLAINTEXT://localhost:9094", "port=9094"})
 class FhirGatewayApplicationTests {
 
   @Test
