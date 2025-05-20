cd989d38fbef2c760c4c439b9afbb66c8f924753
makampf <1058200+makampf@users.noreply.github.com>
2025-05-20 09:22:20 +0200

    test: fix unit tests for refactored producer config (#206)
    * test: adds embedded kafka to test context load sucessfully
    
    * test: fix unit tests for refactored producer config
    
    * doc: adds consumeOnly to table of env config
    
    * chore(style): use single quote instead of double for strings to satisfy groovy linter

--- src/main/java/org/miracum/etl/fhirgateway/processors/KafkaConsumer.java
+++ src/main/java/org/miracum/etl/fhirgateway/processors/KafkaConsumer.java
@@ -3,15 +3,13 @@ package org.miracum.etl.fhirgateway.processors;
 import java.util.function.Consumer;
 import org.hl7.fhir.r4.model.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(value = "services.kafka.enabled")
-@ConditionalOnMissingBean(KafkaProcessor.class)
+@ConditionalOnProperty({"services.kafka.enabled", "services.kafka.consumeOnly"})
 public class KafkaConsumer extends BaseKafkaProcessor {
 
   @Autowired
