178a1309c56c170823d4e4ce2201f838795a9cb0
chgl <chgl@users.noreply.github.com>
2026-04-06 17:52:36 +0200

    fix: Spring boot 4.0 compatibility

--- src/main/java/org/miracum/etl/fhirgateway/processors/KafkaConsumer.java
+++ src/main/java/org/miracum/etl/fhirgateway/processors/KafkaConsumer.java
@@ -2,7 +2,6 @@ package org.miracum.etl.fhirgateway.processors;
 
 import java.util.function.Consumer;
 import org.hl7.fhir.r4.model.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
@@ -13,13 +12,12 @@ import org.springframework.stereotype.Component;
     "${services.kafka.enabled} and ${services.kafka.processor.enabled} and ${services.kafka.processor.consume-only}")
 public class KafkaConsumer extends BaseKafkaProcessor {
 
-  @Autowired
   public KafkaConsumer(ResourcePipeline pipeline) {
     super(pipeline);
   }
 
   @Bean
-  public Consumer<Message<Resource>> process() {
+  Consumer<Message<Resource>> process() {
     return super::process;
   }
 }
