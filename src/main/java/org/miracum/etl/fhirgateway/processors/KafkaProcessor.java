bb15186d4a51eb9622af41d212e68caba5888f23
chgl <chgl@users.noreply.github.com>
2026-04-06 17:53:05 +0200

    refactor: pre-compile regex and cleaner hmac handling

--- src/main/java/org/miracum/etl/fhirgateway/processors/KafkaProcessor.java
+++ src/main/java/org/miracum/etl/fhirgateway/processors/KafkaProcessor.java
@@ -2,10 +2,10 @@ package org.miracum.etl.fhirgateway.processors;
 
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
-import jakarta.annotation.Nullable;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.digest.HmacUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.r4.model.Bundle;
@@ -24,19 +24,23 @@ public class KafkaProcessor extends BaseKafkaProcessor {
 
   private final String generateTopicMatchExpression;
   private final String generateTopicReplacement;
-  private final KafkaProcessorConfig config;
-  @Nullable private HmacUtils hmac;
+  private final Pattern topicPattern;
+  private final Optional<HmacUtils> hmac;
 
   public KafkaProcessor(ResourcePipeline pipeline, KafkaProcessorConfig config) {
     super(pipeline);
     this.generateTopicMatchExpression = config.generateOutputTopic().matchExpression();
     this.generateTopicReplacement = config.generateOutputTopic().replaceWith();
-    this.config = config;
+    this.topicPattern = Pattern.compile(generateTopicMatchExpression);
 
     if (config.cryptoHashMessageKeys().enabled()) {
       hmac =
-          new HmacUtils(
-              config.cryptoHashMessageKeys().algorithm(), config.cryptoHashMessageKeys().key());
+          Optional.of(
+              new HmacUtils(
+                  config.cryptoHashMessageKeys().algorithm(),
+                  config.cryptoHashMessageKeys().key()));
+    } else {
+      hmac = Optional.empty();
     }
   }
 
@@ -50,17 +54,17 @@ public class KafkaProcessor extends BaseKafkaProcessor {
 
       var processed = super.process(message);
 
-      var messageKey = message.getHeaders().getOrDefault(KafkaHeaders.RECEIVED_KEY, "").toString();
-
-      if (config.cryptoHashMessageKeys().enabled() && hmac != null) {
-        messageKey = hmac.hmacHex(messageKey);
-      }
+      var originalMessageKey =
+          message.getHeaders().getOrDefault(KafkaHeaders.RECEIVED_KEY, "").toString();
+      var outputMessageKey =
+          hmac.map(h -> h.hmacHex(originalMessageKey)).orElse(originalMessageKey);
 
       var messageBuilder =
-          MessageBuilder.withPayload(processed).setHeader(KafkaHeaders.KEY, messageKey);
+          MessageBuilder.withPayload(processed).setHeader(KafkaHeaders.KEY, outputMessageKey);
 
       var inputTopic =
-          Objects.requireNonNull(message.getHeaders().get(KafkaHeaders.RECEIVED_TOPIC)).toString();
+          Objects.requireNonNull(
+              message.getHeaders().get(KafkaHeaders.RECEIVED_TOPIC, String.class));
 
       var outputTopic = computeOutputTopicFromInputTopic(inputTopic);
       // see https://github.com/spring-cloud/spring-cloud-stream/issues/1909 and
@@ -75,8 +79,7 @@ public class KafkaProcessor extends BaseKafkaProcessor {
   private Optional<String> computeOutputTopicFromInputTopic(String inputTopic) {
     if (StringUtils.isNotBlank(generateTopicMatchExpression)
         && StringUtils.isNotBlank(generateTopicReplacement)) {
-      var outputTopic =
-          inputTopic.replaceFirst(generateTopicMatchExpression, generateTopicReplacement);
+      var outputTopic = topicPattern.matcher(inputTopic).replaceFirst(generateTopicReplacement);
 
       LOG.debug(
           "Computed output topic using {} and {} from {} as {}",
